99f8c2f2cb36859a59622b1bcbedfd87725ed941
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-03 10:47:50 +0200

    Refactor - haskell setup

--- haskell-pack.el
+++ haskell-pack.el
@@ -12,6 +12,7 @@
                  flymake-hlint
                  smartscan))
 
+(require 'flymake)
 (require 'haskell-mode)
 (require 'inf-haskell)
 
@@ -26,66 +27,21 @@
 ;;;;;;;;;;;;;;;;;;;;; haskell
 
 (defun haskell-pack-mode-defaults ()
+  "Activate default haskell modes."
   (subword-mode +1)
   (turn-on-haskell-doc-mode)
   (turn-on-ghci-completion)
   ;; Ignore compiled Haskell files in filename completions
-  (add-to-list 'completion-ignored-extensions ".hi"))
+  (add-to-list 'completion-ignored-extensions ".hi")
+  (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line))
 
 (add-hook 'haskell-mode-hook 'haskell-pack-mode-defaults)
 
-;;;;;;;;;;;;;;;;;;;;; load the general bindings
-
-;; (defun flymake-haskell-init ()
-;;   "When flymake triggers, generates a tempfile containing the
-;;   contents of the current buffer, runs `hslint` on it, and
-;;   deletes file. Put this file path (and run `chmod a+x hslint`)
-;;   to enable hslint: https://gist.github.com/1241073"
-;;   (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace))
-;;          (local-file  (file-relative-name
-;;                        temp-file
-;;                        (file-name-directory buffer-file-name))))
-;;     (list "hslint" (list local-file))))
-
-;; (defun flymake-haskell-enable ()
-;;   "Enables flymake-mode for haskell, and sets <C-c d> as command
-;;   to show current error."
-;;   (when (and buffer-file-name
-;;              (file-writable-p
-;;               (file-name-directory buffer-file-name))
-;;              (file-writable-p buffer-file-name))
-;;     (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line)
-;;     (flymake-mode t)))
-
 ;; Forces flymake to underline bad lines, instead of fully
 ;; highlighting them; remove this if you prefer full highlighting.
-(custom-set-faces
- '(flymake-errline ((((class color)) (:underline "red"))))
- '(flymake-warnline ((((class color)) (:underline "yellow")))))
-
-;; (defun my-haskell-mode-hook ()
-;;   "hs-lint binding, plus autocompletion and paredit."
-;;   (local-set-key "\C-cl" 'hs-lint)
-;;   (setq ac-sources
-;;         (append '(ac-source-yasnippet
-;;                   ac-source-abbrev
-;;                   ac-source-words-in-buffer
-;;                   my/ac-source-haskell)
-;;                 ac-sources))
-;;   (dolist (x '(haskell literate-haskell))
-;;     (add-hook
-;;      (intern (concat (symbol-name x)
-;;                      "-mode-hook"))
-;;      'turn-on-paredit)))
-
-;; (eval-after-load 'haskell-mode
-;;   '(progn
-;;      (require 'flymake)
-;;      (push '("\\.l?hs\\'" flymake-haskell-init) flymake-allowed-file-name-masks)
-;;      (add-hook 'haskell-mode-hook 'flymake-haskell-enable)
-;;      ;; (add-hook 'haskell-mode-hook 'my-haskell-mode-hook))
-;;      ))
+;; (custom-set-faces
+;;  '(flymake-errline ((((class color)) (:underline "red"))))
+;;  '(flymake-warnline ((((class color)) (:underline "yellow")))))
 
 ;; ######### HELP IN SWITCHING BETWEEN BUFFERS
 
@@ -125,9 +81,6 @@ haskell buffer and the REPL buffer."
             ;; on inf-haskell, C-c C-z is on comint-stop-subjob
             (define-key inferior-haskell-mode-map (kbd "C-c C-z") 'haskell-pack-switch-to-last-haskell-buffer)
             (define-key inferior-haskell-mode-map (kbd "C-j") 'comint-send-input)))
-
-;; (when (require 'haskell-interactive nil 'noerror)
-;;   (define-key haskell-interactive-mode-map (kbd "C-j") 'haskell-interactive-mode-return))
 
 (require 'smartscan)
 (add-hook 'haskell-mode-hook (lambda () (smartscan-mode)))
