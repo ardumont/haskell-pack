16e97e03866a9fd2eff7d2b98bdaa2b234eca72e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 20:32:05 +0200

    Use use-package

--- haskell-pack.el
+++ haskell-pack.el
@@ -6,20 +6,7 @@
 
 ;; haskell-pack
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(flymake
-                                       flymake-shell
-                                       haskell-mode
-                                       ghci-completion
-                                       flymake-hlint
-                                       smartscan
-                                       w3m
-                                       shm
-                                       deferred))
-
-(require 'flymake)
-(require 'haskell-mode)
-(require 'deferred)
+(use-package deferred)
 
 ;; utilities
 
@@ -45,104 +32,100 @@
   "Trigger cabal install of PACKAGES."
   (mapc 'haskell-pack/cabal-install-package packages))
 
-;; structured-haskell-mode setup
-(require 'shm)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+(use-package flymake
+  :config
+  ;; Forces flymake to underline bad lines, instead of fully
+  ;; highlighting them;
+  (custom-set-faces
+   '(flymake-errline ((((class color)) (:underline "red"))))
+   '(flymake-warnline ((((class color)) (:underline "yellow"))))))
 
-;; Install needed cabal packages to be fully compliant with this setup
-(haskell-pack/cabal-install-packages '("structured-haskell-mode"
-                                       "stylish-haskell"
-                                       "hasktags"
-                                       "hlint"))
+(use-package flymake-shell)
+(use-package ghci-completion)
+(use-package w3m)
 
-;; On save, let stylish format code adequately
-(custom-set-variables '(haskell-stylish-on-save t))
+(use-package flymake-hlint
+  :config (haskell-pack/cabal-install-package "hlint"))
 
-;; turn-on-haskell-* are not compatible with structured-haskell-mode (shm)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(use-package shm
+  :config
+  (haskell-pack/cabal-install-package "structured-haskell-mode")
+  (add-hook 'structured-haskell-mode-hook (lambda ()
+                                            ;; buffer map
+                                            (define-key shm-map (kbd "C-i")   'shm/tab)
+                                            (define-key shm-map (kbd "C-M-i") 'shm/backtab)
+                                            (define-key shm-map (kbd "C-m")   'shm/simple-indent-newline-same-col)
+                                            (define-key shm-map (kbd "C-M-m") 'shm/simple-indent-newline-indent)
+                                            (define-key shm-map (kbd "C-h")   'shm/del)
+                                            (define-key shm-map (kbd "C-S-h") 'shm/delete)
+                                            (define-key shm-map (kbd "C-M-h") 'shm/backward-kill-word))))
 
-;; trying out the default font color for the moment
-;; (set-face-background 'shm-current-face "#eee8d5")
-;; (set-face-background 'shm-quarantine-face "lemonchiffon")
+(use-package smartscan)
 
-(defun haskell-pack/structured-haskell-bindings ()
-  "Add some more bindings for the structured-haskell-mode-map."
-  (interactive)
-  ;; buffer map
-  (define-key shm-map (kbd "C-i")   'shm/tab)
-  (define-key shm-map (kbd "C-M-i") 'shm/backtab)
-  (define-key shm-map (kbd "C-m")   'shm/simple-indent-newline-same-col)
-  (define-key shm-map (kbd "C-M-m") 'shm/simple-indent-newline-indent)
-  (define-key shm-map (kbd "C-h")   'shm/del)
-  (define-key shm-map (kbd "C-S-h") 'shm/delete)
-  (define-key shm-map (kbd "C-M-h") 'shm/backward-kill-word))
+(use-package haskell-mode
+  :config
+  ;; Install needed cabal packages to be fully compliant with this setup
+  (haskell-pack/cabal-install-packages '("stylish-haskell"
+                                         "hasktags"))
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+  ;; On save, let stylish format code adequately
+  (custom-set-variables '(haskell-stylish-on-save t))
 
-(add-hook 'structured-haskell-mode-hook 'haskell-pack/structured-haskell-bindings)
+  ;; turn-on-haskell-* are not compatible with structured-haskell-mode (shm)
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+  ;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+  ;; trying out the default font color for the moment
+  ;; (set-face-background 'shm-current-face "#eee8d5")
+  ;; (set-face-background 'shm-quarantine-face "lemonchiffon")
 
-;; compilation on the fly setup
+  (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+  ;; compilation on the fly setup
+  (add-hook 'haskell-mode-hook 'flymake-hlint-load)
+  (add-hook 'haskell-mode-hook (lambda ()
+                                 "Activate default haskell modes."
+                                 (subword-mode +1)
+                                 (turn-on-haskell-doc-mode)
+                                 (turn-on-ghci-completion)
+                                 ;; Ignore compiled Haskell files in filename completions
+                                 (add-to-list 'completion-ignored-extensions ".hi")
+                                 (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line)
+                                 (require 'w3m-haddock)
+                                 (define-key haskell-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock)))
+  (add-hook 'haskell-mode-hook 'smartscan-mode)
 
-(require 'flymake-hlint)
-(add-hook 'haskell-mode-hook 'flymake-hlint-load)
+  ;; Main mode
 
-;;;;;;;;;;;;;;;;;;;;; haskell
+  ;; interactive-haskell-mode is the mode! (inferior-haskell is deprecated)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
-(defun haskell-pack/mode-defaults ()
-  "Activate default haskell modes."
-  (subword-mode +1)
-  (turn-on-haskell-doc-mode)
-  (turn-on-ghci-completion)
-  ;; Ignore compiled Haskell files in filename completions
-  (add-to-list 'completion-ignored-extensions ".hi")
-  (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line)
-  (require 'w3m-haddock)
-  (define-key haskell-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock))
+  (custom-set-variables
+   '(haskell-process-suggest-remove-import-lines t)
+   '(haskell-process-auto-import-loaded-modules t)
+   '(haskell-process-log t)
+   '(haskell-tags-on-save t)
+   '(haskell-process-type 'cabal-repl))
 
-(add-hook 'haskell-mode-hook 'haskell-pack/mode-defaults)
+  ;; (custom-set-variables
+  ;;  '(haskell-process-wrapper-function (lambda (argv) (append (list "nix-shell" "haskell-lab.nix" "--command" )
+  ;;                                                       (list (shell-quote-argument (mapconcat 'identity argv " "))))))
+  ;;  '(haskell-process-type cabal-repl))
 
-;; Forces flymake to underline bad lines, instead of fully
-;; highlighting them;
-(custom-set-faces
- '(flymake-errline ((((class color)) (:underline "red"))))
- '(flymake-warnline ((((class color)) (:underline "yellow")))))
+  (custom-set-variables
+   '(haskell-font-lock-symbols 'unicode)
+   ;; '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans")))
+   ;; not all haskell files needs a setup (.xmonad/xmonad.hs for one), use .dir-locals.el for the moment
+   ;; '(haskell-process-wrapper-function (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
+   ;;                                                      (list (mapconcat 'identity argv " ")))))
+   '(haskell-process-type 'cabal-repl)
+   '(haskell-process-log t))
 
-(require 'smartscan)
-(add-hook 'haskell-mode-hook (lambda () (smartscan-mode)))
-
-;; Main mode
-
-;; interactive-haskell-mode is the mode! (inferior-haskell is deprecated)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
-(custom-set-variables
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-tags-on-save t)
- '(haskell-process-type 'cabal-repl))
-
-;; (custom-set-variables
-;;  '(haskell-process-wrapper-function (lambda (argv) (append (list "nix-shell" "haskell-lab.nix" "--command" )
-;;                                                       (list (shell-quote-argument (mapconcat 'identity argv " "))))))
-;;  '(haskell-process-type cabal-repl))
-
-(custom-set-variables
- '(haskell-font-lock-symbols 'unicode)
- ;; '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans")))
- ;; not all haskell files needs a setup (.xmonad/xmonad.hs for one), use .dir-locals.el for the moment
- ;; '(haskell-process-wrapper-function (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
- ;;                                                      (list (mapconcat 'identity argv " ")))))
- '(haskell-process-type 'cabal-repl)
- '(haskell-process-log t))
-
-;; .dir-locals.el example
-;; ((haskell-mode . ((haskell-process-wrapper-function . (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
-;;                                                          (list (mapconcat 'identity argv " ")))))
-;;                   (haskell-process-type . cabal-repl))))
-
+  ;; .dir-locals.el example
+  ;; ((haskell-mode . ((haskell-process-wrapper-function . (lambda (argv) (append (list "nix-shell" "-I" "." "--command" )
+  ;;                                                          (list (mapconcat 'identity argv " ")))))
+  ;;                   (haskell-process-type . cabal-repl))))
+  )
 
 (provide 'haskell-pack)
 ;;; haskell-pack.el ends here
